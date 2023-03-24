73a938843504a424002e673d2d133a601e8e2384
justinhornung <jhornung328i@gmail.com>
2023-03-23 22:14:19 -0400

    hard code link so indoor plants are called

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,7 +1,12 @@
 class PlantsController < ApplicationController
   def index
-    response = HTTP.get("https://perenual.com/api/species-list?page=1&key=#{ENV["PLANT_API_KEY"]}")
-    articles = response.parse
-    render json: articles
+    # response = HTTP.get("https://perenual.com/api/species-list?page=1&key=#{ENV["PLANT_API_KEY"]}")
+    response = HTTP.get("https://perenual.com/api/species-list?key=sk-8xO964190bf47d56b271&indoor=1")
+    plants = response.parse
+    render json: plants
+  end
+
+  def show
+    response = HTTP.get("https://perenual.com/api/species/details/[ID]?key=sk-8xO964190bf47d56b271")
   end
 end
